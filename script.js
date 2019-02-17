60e96be924f9d3fa2efae412a334241291df9e53
Jill Blaeser <jblaeser01@gmail.com>
2019-02-17 12:58:19 -0600

    var setup

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+var speeches = [
+  {
+    author : "Winston Churchill" ,
+    year : 1940,
+    bce : false
+  },
+  {
+    author: "Ghandi",
+    year: 1942,
+    bce : false
+  },
+  {
+    author: "Demosthenes",
+    year : 342,
+    bce : true
+  }
+];
+
+
+
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
 });
